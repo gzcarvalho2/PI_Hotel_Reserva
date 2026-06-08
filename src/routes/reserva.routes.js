559272874f278a552194e9a64b224b5243603f98
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2026-06-08 17:05:11 -0300

    aa

--- src/routes/reserva.routes.js
+++ src/routes/reserva.routes.js
@@ -1,14 +1,10 @@
 // src/routes/reserva.routes.js
 const ReservaController = require('../controllers/reserva.controller');
 
-// O prefixo base exigido pelo servidor da faculdade
-const BASE_PATH = "/20261prj5/hotel/reserva";
-
-// Exportamos uma função que recebe o 'server' do Restify e regista as rotas
 module.exports = (server) => {
-    server.get(`${BASE_PATH}`, ReservaController.listar); 
-    server.post(`${BASE_PATH}`, ReservaController.criar); 
-    server.get(`${BASE_PATH}/:id`, ReservaController.buscarPorId); 
-    server.put(`${BASE_PATH}/:id`, ReservaController.atualizar); 
-    server.del(`${BASE_PATH}/:id`, ReservaController.deletar);
+    server.get("/reservas", ReservaController.listar); 
+    server.post("/reservas", ReservaController.criar); 
+    server.get("/reservas/:id", ReservaController.buscarPorId); 
+    server.put("/reservas/:id", ReservaController.atualizar); 
+    server.del("/reservas/:id", ReservaController.deletar);
 };
