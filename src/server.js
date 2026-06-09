1f6d4d8d5a8af12083b20c832282d8526c32df74
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2026-06-09 10:52:18 -0300

    Adiciona suporte a CORS e queryParser no servidor

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
 require('dotenv').config();
 const restify = require('restify');
+const corsMiddleware = require("restify-cors-middleware2"); // NOVO IMPORT
 const { connectRabbitMQ } = require('./config/rabbitmq'); 
 const { iniciarConsumidorPagamento } = require('./consumers/pagamento.consumer');
 const { iniciarConsumidorQuarto } = require('./consumers/quarto.consumer');
 const { iniciarConsumidorCliente } = require('./consumers/cliente.consumer');
 
 const server = restify.createServer();
+
+// ==========================================
+// CONFIGURAÇÃO DO CORS
+// ==========================================
+const cors = corsMiddleware({
+  origins: ['*'],           // Permite qualquer origem (Frontend, Postman, outros MS)
+  allowHeaders: ['Authorization', 'Content-Type'],
+  exposeHeaders: ['Authorization'],
+});
+
+// A aplicação do CORS deve acontecer ANTES dos parsers e das rotas
+server.pre(cors.preflight);
+server.use(cors.actual);
+
+// ==========================================
+// MIDDLEWARES PADRÃO
+// ==========================================
+server.use(restify.plugins.queryParser()); // Útil para pegar variáveis na URL (ex: ?id=1)
 server.use(restify.plugins.bodyParser());
 
 // ==========================================
@@ -15,7 +34,9 @@ require('./routes/reserva.routes')(server);
 
 const PORT = 9532;
 
-// ADICIONE O '0.0.0.0' AQUI:
+// ==========================================
+// INICIALIZAÇÃO DO SERVIDOR (com 0.0.0.0 para Docker)
+// ==========================================
 server.listen(PORT, '0.0.0.0', async () => { 
     try {
         console.log(`${server.name} a correr em ${server.url}`); 
