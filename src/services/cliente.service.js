ca29c6a4d6160fb5812ac8c6df9cf6a319232fbf
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2026-06-09 10:45:04 -0300

    aa

--- src/services/cliente.service.js
+++ src/services/cliente.service.js
@@ -6,13 +6,19 @@ const URL_MS_CLIENTE = process.env.CLIENTE_API_URL || 'http://ip_do_colega:porta
 
 const validarCliente = async (cliente_id) => {
     try {
+        console.log(`[AXIOS] Vai perguntar à API Cliente se o ID ${cliente_id} existe...`);
+        console.log(`[AXIOS] URL chamada: ${URL_MS_CLIENTE}/${cliente_id}`);
+
         // Faz a requisição GET à API do colega para buscar o cliente pelo ID
         const resposta = await axios.get(`${URL_MS_CLIENTE}/${cliente_id}`);
         
+        console.log(`[AXIOS] Sucesso! A API do colega respondeu com os dados:`, resposta.data);
         // Se a API responder com sucesso (200 OK), o cliente existe.
         return resposta.data; 
 
     } catch (error) {
+        
+        console.log(`[AXIOS] Falhou! A API respondeu com status:`, error.response?.status);
         console.error(`[Serviço Cliente] Erro ao validar cliente ${cliente_id}:`, error.message);
         // Se der erro 404 (Não Encontrado) ou a API estiver fora do ar
         throw new Error("Cliente não encontrado na base de dados ou Microsserviço de Cliente indisponível.");
